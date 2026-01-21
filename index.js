87dea67d35578a6b7630f44a3226e89cd3897491
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-01-21 15:27:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ io.on("connection", async (socket) => {
 const messages = [
   { id_from: "190", id_to: "205", message: "Hi, Fanny, can you call me.", sent_at:"Yesterday:10:12", seen: "seen"},
   { id_from: "190", id_to: "209", message: "Hi, Jillian, can you call me.", sent_at:"Yesterday:10:13", seen: "seen"},
-  { id_from: "190", id_to: "240", message: "Hi, Karine, can you call me.", sent_at:"Yesterday:10:14",seen: "seen" },
+  { id_from: "190", id_to: "210", message: "Hi, Karine, can you call me.", sent_at:"Yesterday:10:14",seen: "seen" },
   { id_from: "205", id_to: "190", message: "Hi, Alice. I'm here. Do you want something.", sent_at:"Today:10:12", seen: "seen"},
   { id_from: "205", id_to: "190", message: "Do you know ...", sent_at:"Today:10:30", seen: "seen"},
   { id_from: "205", id_to: "190", message: "I've meet Kevin in the school", sent_at:"Today:10:38", seen: "seen"}
