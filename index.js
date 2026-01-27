f538425b00f5be12d8063c8138bcfe951527c30c
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-01-27 19:57:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -121,11 +121,11 @@ io.on("connection", async (socket) => {
 
 const messages = [
   { id_from: "196", id_to: "197", message: "Hi, Fanny, can you call me.", sent_at:"Yesterday:10:12", seen: "seen"},
-  { id_from: "196", id_to: "197", message: "Hi, Jillian, can you call me.", sent_at:"Yesterday:10:13", seen: "seen"},
-  { id_from: "196", id_to: "197", message: "Hi, Karine, can you call me.", sent_at:"Yesterday:10:14",seen: "seen" },
+  { id_from: "196", id_to: "209", message: "Hi, Jillian, can you call me.", sent_at:"Yesterday:10:13", seen: "seen"},
+  { id_from: "196", id_to: "210", message: "Hi, Karine, can you call me.", sent_at:"Yesterday:10:14",seen: "seen" },
   { id_from: "197", id_to: "196", message: "Hi, Alice. I'm here. Do you want something.", sent_at:"Today:10:12", seen: "seen"},
-  { id_from: "197", id_to: "196", message: "Do you know ...", sent_at:"Today:10:30", seen: "seen"},
-  { id_from: "197", id_to: "196", message: "I've meet Kevin in the school", sent_at:"Today:10:38", seen: "seen"}
+  { id_from: "209", id_to: "196", message: "Do you know ...", sent_at:"Today:10:30", seen: "seen"},
+  { id_from: "210", id_to: "196", message: "I've meet Kevin in the school", sent_at:"Today:10:38", seen: "seen"}
 ];
 
   
