4728b783bf9cc539ca27414bff9725f878867c9a
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-03-06 10:15:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -469,10 +469,10 @@ function isUserOnline(userId) {
 async function getUsersList() {
   
   const users = [
-      { id: 272, nickname: "Alice" },
+      { id: 274, nickname: "Alice" },
       { id: 301, nickname: "Bob" },
       { id: 302, nickname: "Charly" },
-      { id: 273, nickname: "Fanny" },
+      { id: 275, nickname: "Fanny" },
       { id: 309, nickname: "Jilian" },
       { id: 310, nickname: "Karine" },
     ];
