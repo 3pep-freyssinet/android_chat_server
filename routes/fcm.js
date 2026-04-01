00c7dda95d0d06982f7b2c045a3bc02296512e5c
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-04-01 14:33:08 +0200

    Update fcm.js

--- routes/fcm.js
+++ routes/fcm.js
@@ -1,1 +1,20 @@
-aa
+// routes/fcm.js
+const express 			= require('express');
+const router  			= express.Router();
+const fcmController	= require('../controllers/fcm_tokens_controller'); // Point to your controller
+const authMiddleware = require('../middleware/auth');
+
+console.log('routes : fcm');
+
+// Define routes
+router.post('/post-all-fcm-tokens',                 fcmController.postAllFCMTokens);           // POST /fcm/post-all-fcm-tokens
+router.get('/get-all-fcm-tokens',                   fcmController.getAllFCMTokens);            // GET /fcm/get-all-fcm-tokens
+//router.post('/store-fcm-tokens',                  fcmController.storeFCMTokens);             // POST /fcm/store-fcm-tokens
+router.post('/store-fcm-token',     authMiddleware, fcmController.storeFCMToken);  // POST /fcm/store-fcm-tokens
+router.post('/revoke-fcm-token',    authMiddleware, fcmController.revokeFCMToken);// POST /fcm/revoke-fcm-tokens
+
+//router.get('/:id', usersController.getUser);                   // GET /users/:id
+//router.put('/:id', usersController.updateUser);                // PUT /users/:id
+
+// Export the router
+module.exports = router;
