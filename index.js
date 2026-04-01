2d21eb4d325b5d4efaaf9a0662ef0f2b9d3bb88d
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-04-01 14:20:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,21 @@ const pool = new Pool({
   idleTimeoutMillis: 1000, 
 });
 
+// Import routes
+//const users_routes  		= require('./routes/users');
+//const tokens_routes 		= require('./routes/tokens');
+const fcm_routes    		= require('./routes/fcm');
+//const notifications_routes    	= require('./routes/notifications');
+//const pins_routes    	        = require('./routes/pins');
+//const environ_routes            = require('./routes/environ');
+
+// Use routes
+app.use('/users', users_routes);
+app.use('/tokens', tokens_routes);
+app.use('/fcm', fcm_routes);
+app.use('/notifications', notifications_routes);
+app.use('/pins', pins_routes);
+app.use('/environ', environ_routes);
 console.log("test");
 
 //test
