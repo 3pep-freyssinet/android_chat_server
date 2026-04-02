2d2ebf53f53b61c87bc37535a93d494d2223ce02
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-04-02 21:58:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -626,13 +626,11 @@ socket.on("chat:get_conversation", async ({ withUserId }) => {
                         
 /////////////////////////////////////////////////////////////////////////////////
 async function getUserName(userId) {
-  console.log("getUserNamen : userId : ", userId);
   try {
     const result = await pool.query(
-      "SELECT nickname FROM users WHERE id = $1",
+      "SELECT nickname FROM chat.users WHERE id = $1",
       [userId]
     );
-console.log("getUserNamen : result.rows : ", result.rows);
     if (result.rows.length === 0) {
       return "Unknown"; // fallback
     }
