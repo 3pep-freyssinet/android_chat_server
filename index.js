bea031e97238a3e65ceb4529889dfe3682dfb87f
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-04-03 11:42:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -644,7 +644,7 @@ async function getUserName(userId) {
   }
 }
 //////////////////////////////////////////////////////////////////
-  function formatMessagePreview(message, maxLength = 40) {
+  function formatMessagePreview(message, maxLength = 37) { //37 + ... = 40
   if (!message) return "";
 
   if (message.message.length <= maxLength) {
