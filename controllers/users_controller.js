8b477c584140cac2f175ee2b32b2b1e41fb57ac2
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-04-18 14:36:37 +0200

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -175,8 +175,8 @@ exports.friendReject = async (req, res) => {
 	  
 	console.log("EMIT TO: ", String(toUserId), "TYPE: ", typeof toUserId);
 	  
-    //io.to(String(toUserId)).emit("friend:request_rejected", { //original
-	io.to(String(fromUserId)).emit("friend:request_rejected", { 
+    io.to(String(toUserId)).emit("friend:request_rejected", { //original
+	//io.to(String(fromUserId)).emit("friend:request_rejected", { 
       fromUserId: fromUserId
     });
 	  
