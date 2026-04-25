ca9f2acbe7875ce4532d5a31aa11112d3f4e3d0a
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-04-25 20:06:28 +0200

    Update socket_state.js

--- socket_state.js
+++ socket_state.js
@@ -8,7 +8,31 @@ admin.initializeApp({
 const onlineUsers = new Map();
 const activeChats = new Map();
 
-const pool = require('../db'); 
+const { Pool } = require('pg');
+
+const admin = require('firebase-admin');
+
+//const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+//admin.initializeApp({
+//  credential: admin.credential.cert(serviceAccount),
+//});
+
+//Render + Aiven + env
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+  client_encoding: 'utf8',
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync("../ca.pem").toString(),
+  },
+  max: 20,
+  min: 1,
+  idleTimeoutMillis: 1000, 
+});
 
 async function getUserName(userId) {
   try {
