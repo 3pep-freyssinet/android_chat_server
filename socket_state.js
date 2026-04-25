93621b9941af04cc47da64226824cd75e0222318
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-04-25 19:33:17 +0200

    Update socket_state.js

--- socket_state.js
+++ socket_state.js
@@ -1,9 +1,15 @@
 // socketState.js
+const admin = require('firebase-admin');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const onlineUsers = new Map();
 const activeChats = new Map();
 
 module.exports = {
   onlineUsers,
-  activeChats
+  activeChats,
+  admin
 };
