d989a90feae92fb50c2eba7a10cf0b88e59755b5
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-04-25 19:52:16 +0200

    Update socket_state.js

--- socket_state.js
+++ socket_state.js
@@ -8,9 +8,25 @@ admin.initializeApp({
 const onlineUsers = new Map();
 const activeChats = new Map();
 
+async function getUserName(userId) {
+  try {
+    const result = await pool.query(
+      "SELECT nickname FROM chat.users WHERE id = $1",
+      [userId]
+    );
+    if (result.rows.length === 0) {
+      return "Unknown"; // fallback
+    }
+    return result.rows[0].nickname;
+  } catch (err) {
+    console.error("Error fetching username:", err);
+    return "Unknown";
+  }
+}
 
 module.exports = {
   onlineUsers,
   activeChats,
-  admin
+  admin,
+  getUserName
 };
