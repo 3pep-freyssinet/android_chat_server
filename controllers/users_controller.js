592d733e2c0aa05735ed4cda51c2b5edbd9e009c
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-05-24 22:54:27 +0200

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -533,7 +533,7 @@ VALUES ($1, $2, $3, $4, $5)
     // -----------------------------------
     // response
     // -----------------------------------
-    console.log('friendsBlock : blockedId : ', blockedId, ' blocked successfully: temporary : ', (expiresAt != null), ' blockedUntil : ', expiresAt ? expiresAt.getTime() : 0 );
+    console.log('friendsBlock : blockedId : ', blockedId, ' blocked successfully: temporary : ', (expiresAt != null), ' blockedUntil : ', expiresAt ? expiresAt : 0 );
     return res.json(
 		{
   			success: true,
