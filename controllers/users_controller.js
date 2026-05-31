eec4ea1e0217a22c15d27df327d866a382743990
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-05-31 22:52:07 +0200

    Update users_controller.js acknowledgedFriendsBlock remove blocked blockPending in io.emit and res

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -608,8 +608,8 @@ exports.acknowledgedFriendsBlock = async (req, res) => {
       "friend:acknowledge-block",
       {
         blockedUserId: meId,
-		blocked: (graceDurationMs != -1),				//temporary block
-        permanentBlockPending: (graceDurationMs == -1),	//permanent block
+		//blocked: (graceDurationMs != -1),				//temporary block
+        //permanentBlockPending: (graceDurationMs == -1),	//permanent block
         acknowledgedAt:now.getTime(),
         graceExpiresAt:graceExpiresAt.getTime()
       }
@@ -623,8 +623,8 @@ exports.acknowledgedFriendsBlock = async (req, res) => {
 
     return res.json({
       success: true,
-	  blocked: (graceDurationMs != -1),					//temporary block
-      permanentBlockPending: (graceDurationMs == -1),	//permanent block
+	  //blocked: (graceDurationMs != -1),					//temporary block
+      //permanentBlockPending: (graceDurationMs == -1),	//permanent block
       acknowledgedAt:now.getTime(),
       graceExpiresAt:graceExpiresAt.getTime()
     });
