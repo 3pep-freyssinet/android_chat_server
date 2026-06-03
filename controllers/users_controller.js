16be6e056b148d86245d0b450d116895b650664e
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2026-06-03 11:57:48 +0200

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -487,7 +487,6 @@ exports.friendsBlock = async (req, res) => {
 		    -- active permanent block workflow
 		    (
 		        expires_at IS NULL
-		        AND permanent_block = TRUE
 		    )
          )
       `,
